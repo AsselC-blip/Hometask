28f3471e15f1a15926fac904e572cb89053f15be
Assel-sketch <73256002+Assel-sketch@users.noreply.github.com>
2023-06-11 00:48:53 +0200

    traingle added

---
+++ Triangle/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("size of one leg");
+            double leg1= double.Parse(Console.ReadLine());
+            Console.WriteLine("size of second leg");
+            double leg2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("size of hypetenuse");
+            double hypetenuse = double.Parse(Console.ReadLine());
+            double result = leg1 * leg2 * hypetenuse;
+            Console.WriteLine("result\n"+result);
+        }
+    }
+}
