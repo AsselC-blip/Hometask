0507f4e614e47e212fe495b854e3ba5f38fbe452
Assel-sketch <73256002+Assel-sketch@users.noreply.github.com>
2023-06-11 00:27:29 +0200

    av added

---
+++ Age verification/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Age_verification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age > 18)
+            {
+                Console.WriteLine("You are adult");
+            }
+            else 
+            {
+                Console.WriteLine("You are not adult");
+            }
+
+        }
+
+    }
+}
