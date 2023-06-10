cf10452a8b4d85515f3f3623635d8e248924e035
Assel-sketch <73256002+Assel-sketch@users.noreply.github.com>
2023-06-11 00:32:46 +0200

    av tern added

---
+++ age verification.tern/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace age_verification.tern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = Convert.ToInt32(Console.ReadLine());
+            string result = age >= 18 ? "You're adult" : "You're not adult";
+            Console.WriteLine(result);
+        }
+    }
+}
