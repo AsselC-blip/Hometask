48d9868404d3818358e52dac8cdb4cf721296b1b
Assel-sketch <73256002+Assel-sketch@users.noreply.github.com>
2023-06-11 00:47:17 +0200

    Season-of-year added

---
+++ Season of year/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Season_of_year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double month;
+            Console.WriteLine("enter month");
+            month = double.Parse(Console.ReadLine());
+
+            switch (month)
+            {
+                case (12 or 1 or 2):
+                    Console.WriteLine("Winter"); break;
+                case (3 or 4 or 5):
+                    Console.WriteLine("Spring"); break;
+                case (6 or 7 or 8):
+                    Console.WriteLine("Summer"); break;
+                case (9 or 10 or 11):
+                    Console.WriteLine("Autumn"); break;
+                default:
+                    break;
+            }
+        }
+    }
+}
